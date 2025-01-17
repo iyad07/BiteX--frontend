4cffc46937a48de7ed94f934a50c8a5fab5a1e87
iyad07 <oideenz4@gmail.com>
2025-01-17 07:01:41 +0000

    edited the "no card added" widget.

--- lib/pages/check_out page/check_out.dart
+++ lib/pages/check_out page/check_out.dart
@@ -37,75 +37,92 @@ class _CheckOutPageState extends State<CheckOutPage> {
       body: Column(
         children: [
           // Payment methods section
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Placeholder for payment methods
-                for (int i = 0; i < 3; i++) paymentMethodtile(),
-              ],
-            ),
-          ),
-          SizedBox(height: 24,),
-          // Card details or "Add new" section
-          Center(
+          Expanded(
+            flex: 1,
             child: Container(
-              height:MediaQuery.sizeOf(context).height/3 ,
-              padding: EdgeInsets.all(16),
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    width: double.infinity,
-                    height: 100,
-                    color: Colors.grey[300], // Placeholder for card image
-                  ),
-                  SizedBox(height: 16),
-                  Container(
-                    width: double.infinity,
-                    height: 16,
-                    color: Colors.grey[300], // Placeholder for text
-                  ),
-                  SizedBox(height: 8),
-                  Container(
-                    width: double.infinity,
-                    height: 16,
-                    color: Colors.grey[300], // Placeholder for subtitle
-                  ),
-                  SizedBox(height: 16),
+                  // Placeholder for payment methods
+                  for (int i = 0; i < 5; i++) paymentMethodtile(),
                 ],
               ),
             ),
           ),
-          // Total and confirm button section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // Card details or "Add new" section
+          Expanded(
+            flex: 6,
+            child: Center(
+              child: Container(
+                height: MediaQuery.sizeOf(context).height / 3,
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'TOTAL:',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
                     Container(
-                      width: 80,
-                      height: 16,
-                      color: Colors.grey[300], // Placeholder for total price
+                      width: double.infinity,
+                      height: 100,
+                      color: Colors.grey[300], // Placeholder for card image
                     ),
+                    SizedBox(height: 16),
+                    Text(
+                      "No card added",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    SizedBox(
+                      width: 266,
+                      child: Text(
+                        "You can add a mastercard and save it for later",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey
+                        ),
+                        textAlign:TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(height: 16),
                   ],
                 ),
-                SizedBox(height: 16),
-                elevatedButton("PAY & CONFIRM", (){})
-              ],
+              ),
+            ),
+          ),
+          // Total and confirm button section
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'TOTAL:',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                        width: 80,
+                        height: 16,
+                        color: Colors.grey[300], // Placeholder for total price
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  elevatedButton("PAY & CONFIRM", () {})
+                ],
+              ),
             ),
           ),
         ],
