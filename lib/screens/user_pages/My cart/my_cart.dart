1c732b30cb44d6c9e73ef4f452f0739f6de4e9ee
iyad07 <oideenz4@gmail.com>
2025-04-27 09:22:45 +0000

    added the user's address to the cart page

--- lib/screens/user_pages/My cart/my_cart.dart
+++ lib/screens/user_pages/My cart/my_cart.dart
@@ -1,6 +1,7 @@
 import 'package:bikex/data/restaurant_handler.dart';
+import 'package:bikex/data/user_provider.dart'; // Add import for UserProvider
 import 'package:bikex/components/buttons.dart';
-import 'package:bikex/components/cred_textfields.dart';
+//import 'package:bikex/components/cred_textfields.dart';
 import 'package:bikex/screens/user_pages/My%20cart/cart.dart';
 import 'package:bikex/screens/user_pages/My%20cart/edit_cart.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ class MyCart extends StatefulWidget {
 
 class _MyCartState extends State<MyCart> {
   bool isEditMode = false;
+  int selectedAddressIndex = 0; // To track which address is selected
 
   void toggleEditMode() {
     setState(() {
@@ -26,6 +28,10 @@ class _MyCartState extends State<MyCart> {
 
   @override
   Widget build(BuildContext context) {
+    // Create an instance of UserProvider to access user data
+    final userProvider = UserProvider();
+    final user = userProvider.demoUser();
+    
     return Consumer<RestaurantHandler>(
       builder: (context, value, child) => Scaffold(
         bottomSheet: Padding(
@@ -41,11 +47,28 @@ class _MyCartState extends State<MyCart> {
                     style: TextStyle(
                         color: Colors.grey, fontWeight: FontWeight.bold),
                   ),
-                  textButton("EDIT", () {})
+                  textButton("CHANGE", () {
+                    // Show address selection dialog
+                    _showAddressSelectionDialog(context, user.address);
+                  })
                 ],
               ),
               SizedBox(height: 8),
-              buildCredTextField("Delivery Address", hasTitle: false),
+              // Display the selected address instead of the text field
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                width: double.infinity,
+                child: Text(
+                  user.address.isNotEmpty 
+                      ? user.address[selectedAddressIndex] 
+                      : "No address available",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
               SizedBox(height: 16),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -60,18 +83,11 @@ class _MyCartState extends State<MyCart> {
                             fontWeight: FontWeight.w400),
                       ),
                       Text(
-                        '\$${value.getTotal().toStringAsFixed(2)}', // Display total with dollar sign and 2 decimal places
+                        '\$${value.getTotal().toStringAsFixed(2)}',
                         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       ),
                     ],
                   ),
-                  /*TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Breakdown >',
-                      style: TextStyle(color: Colors.orange),
-                    ),
-                  ),*/
                 ],
               ),
               SizedBox(height: 12),
@@ -87,6 +103,46 @@ class _MyCartState extends State<MyCart> {
                 onEdit: toggleEditMode,
               ),
       ),
+    );
+  }
+  
+  // Method to show address selection dialog
+  void _showAddressSelectionDialog(BuildContext context, List<String> addresses) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Select Delivery Address"),
+          content: Container(
+            width: double.maxFinite,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: addresses.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(addresses[index]),
+                  leading: Radio<int>(
+                    value: index,
+                    groupValue: selectedAddressIndex,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedAddressIndex = value!;
+                        Navigator.pop(context);
+                      });
+                    },
+                  ),
+                  onTap: () {
+                    setState(() {
+                      selectedAddressIndex = index;
+                      Navigator.pop(context);
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
